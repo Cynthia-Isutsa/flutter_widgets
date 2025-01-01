5458039f9f55a98ea022280e76fb05ccfe806d0d
Cynthia-Isutsa <isutsacynthia@gmail.com>
2025-01-01 22:44:55 +0300

    more widgets

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,63 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body:  Center(
-        child: IconButton(
-           onPressed: () {  }, 
-          icon: const Icon(
-            Icons.alternate_email
-          ),
-          color: Colors.amber
-          ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //verticalDirection: VerticalDirection.down,
+      crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text("This is my year"),
+          TextButton(
+            onPressed: () {  },
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.amber),
+             
+            ),
+            child: const Text( "Click me"),
+            ),
+            Container(
+              color: Colors.cyan,
+              padding: const EdgeInsets.all(30.0),
+              child: const Text("I am a container"),
+            ),
+          const Image(
+            image: AssetImage('assets/pic1.jpg'),
+            height: 100,
+            width: 100,
+            ),
+        ]
+      ),
+
+      // const Padding(
+      //   padding: EdgeInsets.all(90.0),
+      //   child: Text("I love myself, so much"),
+      // ),
+      // Container(
+      //   color: Colors.grey[400],
+      //   margin: const EdgeInsets.all(20.0),
+      //   padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //   child: const Text(
+      //     "I love myself",
+      //     style: TextStyle(
+      //       fontWeight: FontWeight.bold,
+      //     )
+      //   ),
+        // child: const Center(
+        //   child: Image(
+        //     image: AssetImage('assets/pic1.jpg'),
+        //     height: 200,
+        //     width: 200,
+        //   ),
+        // )
+     // ),
+      // Center(
+      //   child: IconButton(
+      //      onPressed: () {  }, 
+      //     icon: const Icon(
+      //       Icons.alternate_email
+      //     ),
+      //     color: Colors.amber
+      //     ),
         // ElevatedButton.icon(
         //  onPressed: () {  
         //  // print("I have clicked");
@@ -38,7 +87,7 @@ class Home extends StatelessWidget {
         // ),
         // ),
    
-         ),
+        // ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
       backgroundColor: Colors.red[600],
